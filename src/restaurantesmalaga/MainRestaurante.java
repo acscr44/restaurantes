fd176dc7eb83b967ae09cc46384911676f1c3fa7
Alejandro <acastillo44@gmail.com>
2023-06-22 09:28:54 +0200

    Warnings supress

--- src/restaurantesmalaga/MainRestaurante.java
+++ src/restaurantesmalaga/MainRestaurante.java
@@ -7,12 +7,12 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
+
 import java.util.List;
 import java.util.Random;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
+
+
 
 import restaurantesmalaga.model.ComparadorRestaurantes;
 import restaurantesmalaga.model.Restaurante;
